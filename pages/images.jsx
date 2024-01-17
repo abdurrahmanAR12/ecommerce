41a8d438c1f2f74ab8652968a146aec21a4b94b9
Abdur Rahman <arishere93@gmail.com>
2024-01-17 14:08:07 +0500

    npm

--- pages/images.jsx
+++ pages/images.jsx
@@ -10,17 +10,17 @@ export async function getServerSideProps(ctx) {
         let img = await Image.findOne({ route: id });
         if (!img)
             return sendRespnonseJson404(res, "Not found");
-        let stream = require("stream"),
-            // compressedBuffer = await imagemin.buffer(img.data, {
-            //     plugins: [
-            //         imageminPngquant({
-            //             quality: [0.6, 0.8]
-            //         })
-            //     ]
-            // });
-            data = await sharp(img.data).resize({ width: req.query['width'] || 1920, height: req.query['height'] || 1080, fit: "contain" }).toBuffer()
+        let stream = require("stream")
+        // compressedBuffer = await imagemin.buffer(img.data, {
+        //     plugins: [
+        //         imageminPngquant({
+        //             quality: [0.6, 0.8]
+        //         })
+        //     ]
+        // });
+        // data = await sharp(img.data).resize({ width: req.query['width'] || 1920, height: req.query['height'] || 1080, fit: "contain" }).toBuffer()
         let pipeline = stream.Readable.from(Buffer.from(data));
-        console.log(data.byteLength)
+        // console.log(data.byteLength)
         pipeline.pipe(res);
         return { props: {} }
     }
